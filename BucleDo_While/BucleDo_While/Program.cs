68b8e4fc8c654c81bb20ed82146bc54bcd9067f3
Juan Acevedo <Juan.Acevedo@teaminternational.com>
2021-10-12 19:10:22 -0500

    Bucle Do While C#

---
+++ BucleDo_While/BucleDo_While/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BucleDo_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int z = 10;
+              do                                             //Primero se ejecuta el do
+              {
+                  Console.WriteLine("Impresion " + z);
+                  z++;
+
+              } while (z < 15);                              //Y luego se verifica la condicion si es verdadera se repite.
+            */
+
+            //Ejercicio
+            Random numero = new Random();
+
+            int aleatorio = numero.Next(0, 100);
+
+            int miNumero;
+
+            int intentos = 0;
+
+            Console.WriteLine("Introduce un numero entre 0 y 100");
+
+            do
+            {
+                intentos++;
+
+                miNumero = int.Parse(Console.ReadLine());
+
+                if (miNumero > aleatorio) Console.WriteLine("El numero es mas bajo");
+
+                if (miNumero < aleatorio) Console.WriteLine("El numero es mas alto");
+
+            } while (aleatorio != miNumero);
+
+            Console.WriteLine("Correcto, te ha tomado " + intentos + " intentos");
+
+         }
+    }
+}
