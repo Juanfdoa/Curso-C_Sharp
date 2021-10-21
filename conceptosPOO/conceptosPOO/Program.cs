f4c5e94444279bad912a90d21758e15c3eb769b2
Juan Acevedo <Juan.Acevedo@teaminternational.com>
2021-10-21 10:28:36 -0500

    POO C# conceptos,operador Math, clases anonimas

---
+++ conceptosPOO/conceptosPOO/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using static System.Math; //Importar toda la clase Math
+// No es recomendable
+using static System.Console; //Importar consola
+
+
+
+namespace conceptosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //realizarTarea();
+
+            double raiz = Math.Sqrt(9);      //Operador matematico
+
+            double potencia = Pow(3, 4);     //Solo usamos pow, ya que impotamos la clase Math.
+
+            Console.WriteLine(raiz);
+            Console.WriteLine(potencia);
+
+            //CLASES ANONIMAS
+            //se utiliza la palabra reservada var, y no se le asigna tipo
+            var miVariable = new { Nombre = "Juan", Edad = 19 };
+
+            Console.WriteLine(miVariable.Nombre + " " + miVariable.Edad);
+        }
+
+        static void realizarTarea()
+        {
+            Punto origen = new Punto();        //ficheros fuente
+                                               //Crea un constructor en una clase diferente
+            Punto destino = new Punto(128, 80);
+
+            Punto medio = new Punto();
+
+            double distancia2 = origen.Distancia(destino);
+
+            Console.WriteLine("La distancia entre los puntos X y Y es: " + distancia2);
+
+            Console.WriteLine($"Numero de objectos creados: {Punto.ContadorDeObjectos()}");
+        }
+    }
+}
---
+++ conceptosPOO/conceptosPOO/Punto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conceptosPOO
+{
+    class Punto
+    {
+        public Punto(int x, int y)
+        {
+            this.x = x;
+
+            this.y = y;
+
+            contadorDeObjectos++;
+        }
+
+        public Punto()
+        {
+            this.x = 0;
+
+            this.y = 0;
+
+            contadorDeObjectos++;
+
+        }
+
+        public double Distancia(Punto otroPunto)
+        {
+            int xDif = this.x - otroPunto.x;
+
+            int yDif = this.y - otroPunto.y;
+
+            double DistaciaPuntos = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(yDif, 2));
+
+            return DistaciaPuntos;
+        }
+
+        public static int ContadorDeObjectos() => contadorDeObjectos;  //para devolver el valor de una variable static
+
+        private int x, y;
+
+        //VARIABLES STATIC.
+        //Estas varibles pueden ser compartidas pero no pueden
+        //ser modificadas por ningun objecto, solo la puede modificar la clase dueña
+
+        private static int contadorDeObjectos = 0;
+    }
+}
