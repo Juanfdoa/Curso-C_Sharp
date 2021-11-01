81acb59efba712a6bbc356993f219d472e12ce88
Juan Acevedo <Juan.Acevedo@teaminternational.com>
2021-11-01 10:50:09 -0500

    Dependency Properties

---
+++ WpfDependencyProperties/WpfDependencyProperties/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDependencyProperties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Registrar dependency propertie
+
+        public int myPropertie
+        {
+            get { return (int)GetValue(MyDependencyPopertie); }
+
+            set { SetValue(MyDependencyPopertie, value);  }
+        }
+
+        public static readonly DependencyProperty MyDependencyPopertie =
+            DependencyProperty.Register("myPropertie", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
